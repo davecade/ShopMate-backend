d3b2cd4e3e3a8faaf3a45c8c0bd40192552cba52
Dave Cadelina <dave.cadelina@reckon.com>
2024-01-12 01:17:26 +1100

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Enable CORS for all origins
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log('Server running on port 3000');
